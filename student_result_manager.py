f2683a41fa3c4d045e457da40f6fc2d4aa19fc9b
Ayaandagar1 <112718461+Ayaandagar1@users.noreply.github.com>
2026-05-25 23:42:03 +0530

    Add files via upload

---
+++ student_result_manager.py
@@ -0,0 +1,50 @@
+student = {}
+
+while True:
+    print("\n-----STUDENT MANAGER APP-----")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Check Result")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    #Add Student
+    if choice == "1":
+        name = input("Enter student name: ")
+        marks = marks(input("Enter marks: "))
+        student[name] = marks
+        print(f"{name} Successfully Added!")
+
+        #view students
+    elif choice == "2":
+        if not student:
+            print("No student found!")
+        else:
+            for name, marks in student.items():
+                print(name, ":", marks)
+
+                #check result
+    elif choice == "3":
+        name = input("Enter student name:")
+
+        if name in student:
+            marks = student[name]
+
+            if marks >= 40:
+                print("pass")
+            else:
+                print("Fail")
+
+        else:
+            print("Student Not found")
+
+            #exit
+    elif choice == "4":
+        print ("Exiting....")
+        break
+
+    else:
+        print("In-valid input")
+
+
